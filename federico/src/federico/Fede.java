2310b442323c8ed8cf3ac37bd1148a3431c06922
adrianplanetas <dw1acovid@gmail.com>
2021-05-21 11:48:49 +0100

    menudaporonga

--- federico/src/federico/Fede.java
+++ federico/src/federico/Fede.java
@@ -9,7 +9,7 @@ public class Fede {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println();
 	}
 	public boolean visualizarSalidda(boolean acertada, int i, String newParam) {
 		return acertada;
