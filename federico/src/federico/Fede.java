2cd322a4512fb1b341b6f8261e5da6bf6f117085
adrianplanetas <dw1acovid@gmail.com>
2021-05-21 11:48:15 +0100

    inicio

---
+++ federico/src/federico/Fede.java
@@ -0,0 +1,19 @@
+package federico;
+
+public class Fede {
+	
+	public boolean visualizarSalida(boolean acertada, int i, String newParam) {
+		return acertada;
+		
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public boolean visualizarSalidda(boolean acertada, int i, String newParam) {
+		return acertada;
+		
+		}
+
+}
